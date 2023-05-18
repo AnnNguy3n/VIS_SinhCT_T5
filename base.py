ed07c17c0b0a697957b044bd8f13c5376b01fecd
AnnNguy3n <annguyen@vis-laboratory.com>
2023-05-18 11:38:55 +0700

    Thêm ValueARG vào danh sách các cột không được coi là biến

--- base.py
+++ base.py
@@ -6,7 +6,7 @@ import os
 class Base:
     def __init__(self, data: pd.DataFrame, path: str) -> None:
         # Các cột bắt buộc
-        dropped_cols = ["TIME", "PROFIT", "SYMBOL", "ValueARG"]
+        dropped_cols = ["TIME", "PROFIT", "SYMBOL", "VALUEARG"]
         for col in dropped_cols:
             if col not in data.columns:
                 raise Exception(f"Thiếu cột {col}")
